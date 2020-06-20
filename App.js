500f8a1d9a2d6a214771d68e8c4d5d709f88e0b3
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-06-20 10:25:48 +0700

    init

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <NavigationContainer>
+
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+export default App;
